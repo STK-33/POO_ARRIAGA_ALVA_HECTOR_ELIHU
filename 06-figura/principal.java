e638a6146b3b8d922687b660f69eee60ec040d60
USUARIO <USUARIO@OBM_P2_003.ssp.cdmx.gob.mx>
2021-11-16 12:24:35 -0600

    ejercicios-cases

---
+++ 06-figura/principal.java
@@ -0,0 +1,9 @@
+public class principal {
+    public static void main(String[] args){
+        //metodo principal
+        //instancia de la clase
+        figura objfigura = new figura();
+        objfigura.menu();
+    }
+    
+}
