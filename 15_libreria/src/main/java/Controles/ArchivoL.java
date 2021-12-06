98bb9b3c8fcd3bdbe9ad5f6d017f68b52af1290e
STK-33 <he.arriaga.a@edu.utc.mx>
2021-12-06 13:01:34 -0600

    ejercicio

---
+++ 15_libreria/src/main/java/Controles/ArchivoL.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controles;
+
+import Documentos.CLibro;
+import java.util.ArrayList;
+
+
+
+//implementar la serializacion de los archivos
+class ArchivoL {
+
+    ArrayList<CLibro> leer() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ 15_libreria/src/main/java/Documentos/CLibro.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Documentos;
+
+/**
+ *
+ * @author PS-33
+ */
+public class CLibro {
+
+    public void aceptarDatos() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
